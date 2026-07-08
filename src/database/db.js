5686eb509ae5dd40033f545d84ff3367e166b162
profchauchuty <cesar.chauchuty@escola.pr.gov.br>
2026-07-08 11:26:10 -0300

    add: init project

---
+++ src/database/db.js
@@ -0,0 +1,35 @@
+const db = {
+    perfil: {
+        nomeCompleto: 'Cesar M. Chauchuty',
+        dataNasc: '1992-11-19',
+        curso: 'Técnico em Desenvolvimento de Sistemas'
+    },
+    certificados: [
+        {
+            nome: 'Curso Aluno - Front-end 100%',
+            dataEmissao: '2026-06-06',
+            descricao: 'Uma descrição... blablba',
+            duracao: 32,
+        },
+        {
+            nome: 'Curso Aluno - Back-end 100%',
+            dataEmissao: '2026-04-06',
+            descricao: 'Uma descrição... blablba',
+            duracao: 48,
+        },
+        {
+            nome: 'Curso Aluno - Banco de Dados 99%',
+            dataEmissao: '2026-02-06',
+            descricao: 'Uma descrição... blablba',
+            duracao: 56,
+        },
+        {
+            nome: 'Curso Aluno - Analise e Projeto de Sistemas 99%',
+            dataEmissao: '2026-01-06',
+            descricao: 'Uma descrição... blablba',
+            duracao: 56,
+        }
+    ]
+}
+
+export default db
